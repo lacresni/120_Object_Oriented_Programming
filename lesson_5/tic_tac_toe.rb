4467ff07aa3d95f748b040eb7251096b8b3fb3a6
Nicolas Lacressonniere <nicolas.lacressonniere@gmail.com>
2018-05-06 22:26:06 +0200

    Change detect_winner method name to winning_marker

--- lesson_5/tic_tac_toe.rb
+++ lesson_5/tic_tac_toe.rb
@@ -27,7 +27,7 @@ class Board
   end
 
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
 
   def count_human_marker(squares)
@@ -39,7 +39,7 @@ class Board
   end
 
   # returns winning marker or nil
-  def detect_winner
+  def winning_marker
     WINNING_LINES.each do |line|
       squares = @squares.values_at(*line)
       if count_human_marker(squares) == 3
@@ -147,7 +147,7 @@ class TTTGame
   def display_result
     clear_screen_and_display_board
 
-    case board.detect_winner
+    case board.winning_marker
     when human.marker
       puts "You won!"
     when computer.marker
