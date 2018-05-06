24bd6feb51b919de766b831d05f0f2f2d6e00a16
Nicolas Lacressonniere <nicolas.lacressonniere@gmail.com>
2018-05-06 22:22:54 +0200

    Add clear_screen_and_display_board method

--- lesson_5/tic_tac_toe.rb
+++ lesson_5/tic_tac_toe.rb
@@ -106,9 +106,12 @@ class TTTGame
     system 'clear'
   end
 
-  def display_board(clear_screen: true)
-    clear if clear_screen
-    puts "screen cleared" if clear_screen
+  def clear_screen_and_display_board
+    clear
+    display_board
+  end
+
+  def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
     puts "     |     |"
@@ -142,7 +145,7 @@ class TTTGame
   end
 
   def display_result
-    display_board
+    clear_screen_and_display_board
 
     case board.detect_winner
     when human.marker
@@ -171,7 +174,7 @@ class TTTGame
     display_welcome_message
 
     loop do
-      display_board(clear_screen: false)
+      display_board
 
       loop do
         human_moves
@@ -180,7 +183,7 @@ class TTTGame
         computer_moves
         break if board.someone_won? || board.full?
 
-        display_board
+        clear_screen_and_display_board
       end
       display_result
       break unless play_again?
