ad07955f55e5db4aad9364f0e4575d5befe16856
Nicolas Lacressonniere <nicolas.lacressonniere@gmail.com>
2018-05-13 08:21:53 +0200

    Add Method Lookup Part 1 exercise

---
+++ oo_exercises/oo_basics_inheritance/method_lookup_part1.rb
@@ -0,0 +1,28 @@
+# Using the following code, determine the lookup path used when invoking
+# cat1.color. Only list the classes that were checked by Ruby when searching
+# for the #color method.
+
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new('Black')
+cat1.color
+
+=begin
+Cat => no method color
+Animal => OK
+
+We can get the lookup path by calling #ancestors method on the class like this:
+puts Cat.ancestors
+=end
