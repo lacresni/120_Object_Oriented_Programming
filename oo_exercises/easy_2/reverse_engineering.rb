9704987af480b745c1646b2489145cbeaef6afaf
Nicolas Lacressonniere <nicolas.lacressonniere@gmail.com>
2018-05-15 22:22:34 +0200

    Add Reverse Engineering exercise

---
+++ oo_exercises/easy_2/reverse_engineering.rb
@@ -0,0 +1,21 @@
+# Write a class that will display:
+#   ABC
+#   xyz
+class Transform
+  def initialize(str)
+    @string = str
+  end
+
+  def uppercase
+    @string.upcase
+  end
+
+  def self.lowercase(str)
+    str.downcase
+  end
+end
+
+# when the following code is run:
+my_data = Transform.new('abc')
+puts my_data.uppercase
+puts Transform.lowercase('XYZ')
